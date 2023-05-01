4b22120e4ef53f27990f19a2116995d04376bb11
EC2 Default User <ec2-user@ip-172-31-41-101.ap-northeast-1.compute.internal>
2023-05-01 06:40:16 +0000

    [Finish] cnr24

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,7 +2,7 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   # 投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params)
@@ -15,20 +15,20 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
     redirect_to post_images_path
   end
-  
+
   # 投稿データのストロングパラメータ
   private
 
